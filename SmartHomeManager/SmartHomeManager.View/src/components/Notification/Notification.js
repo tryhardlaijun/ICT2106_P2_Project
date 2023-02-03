9b2f3ee65484848c8f9e528e7bb71b6d3560d6ab
jianweiiii <jianweii97@hotmail.com>
2023-02-03 17:37:23 +0800

    added popup for notification

--- SmartHomeManager/SmartHomeManager.View/src/components/Notification/Notification.js
+++ SmartHomeManager/SmartHomeManager.View/src/components/Notification/Notification.js
@@ -1,21 +1,43 @@
 import React, { useState } from "react";
 import {BellIcon} from '@chakra-ui/icons'
-import { Box } from "@chakra-ui/react";
+import NotificationPopup from "./NotificationPopup";
+import {
+    Popover,
+    PopoverTrigger,
+    PopoverContent,
+    PopoverHeader,
+    PopoverBody,
+    PopoverFooter,
+    PopoverArrow,
+    PopoverCloseButton,
+    PopoverAnchor,
+    Flex,
+    Box, 
+    useDisclosure
+} from '@chakra-ui/react'
 
 
-export default function Notification() {
+export default function Notification({open}) {
+
+    const {isOpen, onClose, onToggle} = useDisclosure()
+
     return (
         <>
-            <div>
+            
+            <Popover>
+                <PopoverTrigger>                  
+                    <BellIcon w={7} h={7} mr={4} onClick={onToggle}/>
+                </PopoverTrigger>
+            
+            <PopoverContent>
                 
-                <BellIcon w={7} h={7} mr={4}/>
-                {/* <Box>
-                    <div>
-                        heading
-                    </div>
-                </Box> */}
-                
-            </div>
+                <PopoverHeader>Welcome back xxxx</PopoverHeader>
+                <PopoverBody>
+                    <NotificationPopup/>
+                </PopoverBody>
+            </PopoverContent>
+            </Popover>
+            
         </>
         )
         
---
+++ SmartHomeManager/SmartHomeManager.View/src/components/Notification/NotificationPopup.js
@@ -0,0 +1,22 @@
+import React, {useState} from "react";
+import { Box } from "@chakra-ui/react";
+
+
+
+
+
+export default function NotificationPopup(){
+
+
+    
+    return(
+        <div>
+            
+                <div >
+                    hi outside of navbar
+                </div>
+            
+
+        </div>
+    )
+}
