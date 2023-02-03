223b7d505432e418d44617e3c42d19d2bc5e545f
jianweiiii <jianweii97@hotmail.com>
2023-02-03 16:28:13 +0800

    added bell icon to navbar

--- SmartHomeManager/SmartHomeManager.View/src/components/NavBar.js
+++ SmartHomeManager/SmartHomeManager.View/src/components/NavBar.js
@@ -62,6 +62,8 @@ export function NavBar() {
           </Flex>
         </Flex>
 
+
+        <Notification />
         <Stack
           flex={{ base: 1, md: 0 }}
           justify={"flex-end"}
@@ -91,7 +93,7 @@ export function NavBar() {
             Sign Up
             </Button>
 
-            <Notification />
+            
         </Stack>
       </Flex>
 
--- SmartHomeManager/SmartHomeManager.View/src/components/Notification/Notification.js
+++ SmartHomeManager/SmartHomeManager.View/src/components/Notification/Notification.js
@@ -1,11 +1,20 @@
 import React, { useState } from "react";
+import {BellIcon} from '@chakra-ui/icons'
+import { Box } from "@chakra-ui/react";
 
 
 export default function Notification() {
     return (
         <>
             <div>
-            hi notification
+                
+                <BellIcon w={7} h={7} mr={4}/>
+                {/* <Box>
+                    <div>
+                        heading
+                    </div>
+                </Box> */}
+                
             </div>
         </>
         )
