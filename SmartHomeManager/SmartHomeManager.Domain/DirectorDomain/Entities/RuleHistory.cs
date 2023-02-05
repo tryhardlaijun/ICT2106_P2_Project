5c0ba26542a5d4782eb1253fc060a6a8fabfe415
Jin Zhenglong <jzlong99@gmail.com>
2023-02-05 14:50:15 +0800

    Updated DBContext
    Added History and RuleHistory to db
    Update structure of RuleHistory

--- SmartHomeManager/SmartHomeManager.Domain/DirectorDomain/Entities/RuleHistory.cs
+++ SmartHomeManager/SmartHomeManager.Domain/DirectorDomain/Entities/RuleHistory.cs
@@ -1,26 +1,25 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace SmartHomeManager.Domain.DirectorDomain.Entities
+namespace SmartHomeManager.Domain.DirectorDomain.Entities;
+
+public class RuleHistory
 {
-    public class RuleHistory
-    {
-        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public Guid RuleHistoryId { get; set; }
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public Guid RuleHistoryId { get; set; }
 
-        public DateTime? StartTime { get; set; }
+    public DateTime? StartTime { get; set; }
 
-        public DateTime? EndTime { get; set; }
+    public DateTime? EndTime { get; set; }
 
-        public string? ActionTrigger { get; set; }
+    public string? ActionTrigger { get; set; }
 
-        [Required]
-        public string ScenarioName { get; set; }
+    [Required] public int RuleNum { get; set; }
 
-        [Required]
-        public int ConfigurationValue { get; set; }
+    [Required] public string ScenarioName { get; set; }
 
-        [Required]
-        public string DeviceName { get; set; }
-    }
+    [Required] public int ConfigurationValue { get; set; }
+
+    [Required] public string DeviceName { get; set; }
 }
