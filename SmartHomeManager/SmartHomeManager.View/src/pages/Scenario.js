6e548e8d589558fe696c11fe6ed4f4f6132881a9
tryhardlaijun <=>
2023-02-06 20:11:12 +0800

    Add scenario.js

---
+++ SmartHomeManager/SmartHomeManager.View/src/pages/Scenario.js
@@ -0,0 +1,53 @@
+import {
+    Button,
+    Table,
+    Thead,
+    Tbody,
+    Tfoot,
+    Tr,
+    Th,
+    Td,
+    TableCaption,
+    TableContainer,
+    Box
+} from '@chakra-ui/react'
+import React from "react";
+
+
+export default function Scenarios() {
+    return <Box padding='4'>
+        <TableContainer>
+        <Table variant='striped' colorScheme='teal'>
+            <TableCaption>Imperial to metric conversion factors</TableCaption>
+            <Thead>
+                <Tr>
+                    <Th>Rule Name</Th>
+                    <Th>Device Name</Th>
+                        <Th>Descrption</Th>
+                        <Th>Options</Th>
+                </Tr>
+            </Thead>
+            <Tbody>
+                <Tr>
+                    <Td>inches</Td>
+                    <Td>millimetres (mm)</Td>
+                    <Td >25.4</Td>
+                    <Td>
+                        <Button ml={2} colorScheme='green' >
+                            Add
+                        </Button>
+                        <Button ml={2} colorScheme='blue' >
+                            Edit
+                        </Button>
+                        <Button ml={2} colorScheme='red' >
+                            Delete
+                        </Button>
+                    </Td>   
+                </Tr>
+
+
+            </Tbody>
+        </Table>
+        </TableContainer>
+    </Box>;
+}
