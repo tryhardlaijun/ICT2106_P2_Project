e2b541a983f12c1a58a6d197763bb2e28507da9c
junweilam <91267859+junweilam@users.noreply.github.com>
2023-02-07 00:28:58 +0800

    modification to charts

--- SmartHomeManager/SmartHomeManager.View/src/components/Analytics/CarbonChart.js
+++ SmartHomeManager/SmartHomeManager.View/src/components/Analytics/CarbonChart.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Chart from "chart.js/auto";
 import { Line } from "react-chartjs-2";
+import { getByPlaceholderText } from '@testing-library/react';
 
 
 const labels = ["January", "February", "March", "April", "May", "June"];
@@ -19,14 +20,61 @@ const data = {
       backgroundColor: "rgb(124,252,0)",
       borderColor: "rgb(124,252,0)",
       data: [2, 4, 6, 10, 34, 60],
-    }
+    },
   ],
 };
+
+const options = {
+  legend:{
+    labels:{
+      font:{
+        size:30,
+      }
+    }
+  },
+  scales: {
+      x: {
+          beginAtZero: true,
+          title:{
+              display: true,
+              text: "Month",
+              font:{
+                size: 30,
+                weight: "bold",
+              }
+          },
+          ticks: {
+              font: {
+                  size: 30,
+              },
+          },
+      },
+      y:{
+        beginAtZero: true,
+        title:{
+          display: true,
+          text: "Carbon Footprint",
+          font:{
+            size: 30,
+            weight: "bold",
+          }
+      },
+        ticks: {
+          font: {
+              size: 30,
+          }
+      }
+      },
+
+  }
+};
+
+
 
 function CarbonChart() {
 
   return (
-    <Line data={data} />
+    <Line options={options} data={data} />
   )
 }
 
