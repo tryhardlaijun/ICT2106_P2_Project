ab5065fffc60453e03f3861da06e3d16fbc9189d
Fabian <fabianchua6@gmail.com>
2023-02-07 13:26:03 +0800

    fix App.js oops

--- SmartHomeManager/SmartHomeManager.View/src/App.js
+++ SmartHomeManager/SmartHomeManager.View/src/App.js
@@ -1,10 +1,12 @@
-import React from "react";
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import React from 'react'
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 
-import { NavBar } from "./components/NavBar";
-import Home from "./pages/Home";
-import Devices from "./pages/Devices";
-import Profiles from "./pages/Profiles";
+import { NavBar } from './components/NavBar'
+import Home from './pages/Home'
+import Devices from './pages/Devices'
+import Profiles from './pages/Profiles'
+import Rooms from './pages/Rooms'
+import { Container } from '@chakra-ui/react'
 
 export function App() {
 	return (
