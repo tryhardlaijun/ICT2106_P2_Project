9ef13fde98ddf14261c231928908d6272decea59
personryan <mageryan1@gmail.com>
2023-02-07 15:26:45 +0800

    Code: Added new profiles page and profile cards

--- SmartHomeManager/SmartHomeManager.View/src/App.js
+++ SmartHomeManager/SmartHomeManager.View/src/App.js
@@ -9,6 +9,8 @@ import Register from "./pages/account/Register";
 import ForgetPassword from "./pages/account/ForgetPassword";
 import Login from "./pages/account/Login";
 import RegisterOK from "./pages/account/RegisterOK";
+import UserProfileEdit from "./pages/profile/EditProfile";
+import ProfileLanding from "./pages/profile/ProfileLanding"
 
 export function App() {
   return (
@@ -23,6 +25,8 @@ export function App() {
                   <Route path="/forgetpw" element={<ForgetPassword />} />
                   <Route path="/login" element={<Login />} />
                   <Route path="/account-created" element={<RegisterOK />} />
+                  <Route path="/edit-profile" element={<UserProfileEdit />} />
+                  <Route path="/profile-landing" element={<ProfileLanding />} />
         </Routes>
       </Router>
     </>
