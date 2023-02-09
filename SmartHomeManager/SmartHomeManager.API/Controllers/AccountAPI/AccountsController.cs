d6604f6f20baf3761f5bf6d698a2baf99b3c92b1
FS75 <juleusseah@gmail.com>
2023-02-10 00:09:41 +0800

    Edited accounts controller

--- SmartHomeManager/SmartHomeManager.API/Controllers/AccountAPI/AccountsController.cs
+++ SmartHomeManager/SmartHomeManager.API/Controllers/AccountAPI/AccountsController.cs
@@ -40,16 +40,18 @@ namespace SmartHomeManager.API.Controllers.AccountController
         }*/
 
         // GET: api/Accounts
-        /*[HttpGet]
+        [HttpGet]
         public async Task<ActionResult<IEnumerable<Account>>> GetAccounts()
         {
-            if (_accountRepository.GetAllAsync() == null)
+            IEnumerable<Account> accounts = await _accountService.GetAccounts();
+
+            if (accounts == null)
             {
                 return NotFound();
             }
-            *//*return await _accountRepository.GetAllAsync();*//*
-            return await _accountRepository.GetAllAsync();
-        }*/
+
+            return Ok(accounts);
+        }
 
         // GET: api/Accounts/5
         [HttpGet("{id}")]
