fadc3cdd6b85db3f7b7e34eec8f91ffc2f057d5f
zoee-Y <44394845+zoee-Y@users.noreply.github.com>
2023-03-11 17:20:38 +0800

    add IBackupRule/ScenarioRepository

---
+++ SmartHomeManager/SmartHomeManager.Domain/BackupDomain/Interfaces/IBackupRuleRepository.cs
@@ -0,0 +1,17 @@
+﻿using SmartHomeManager.Domain.BackupDomain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHomeManager.Domain.BackupDomain.Interfaces
+{
+    public interface IBackupRuleRepository
+    {
+        public Task<IEnumerable<BackupRule>> GetAllBackupRule();
+        public Task<List<BackupRule>> GetBackupRuleById(Guid scenarioId);
+        public Task<bool> CreateBackupRule(BackupRule backupRule);
+        public Task<bool> DeleteBackupRule(BackupRule backupRule);
+    }
+}
---
+++ SmartHomeManager/SmartHomeManager.Domain/BackupDomain/Interfaces/IBackupScenarioRepository.cs
@@ -0,0 +1,18 @@
+﻿using SmartHomeManager.Domain.BackupDomain.Entities;
+using SmartHomeManager.Domain.SceneDomain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHomeManager.Domain.BackupDomain.Interfaces
+{
+    public interface IBackupScenarioRepository
+    {
+        public Task<IEnumerable<BackupScenario>> GetAllBackupScenario();
+        public Task<List<BackupScenario>> GetBackupScenarioById(Guid scenarioId);
+        public Task<bool> CreateBackupScenario(BackupScenario backupScenario);
+        public Task<bool> DeleteBackupScenario(BackupScenario backupScenario);
+    }
+}
